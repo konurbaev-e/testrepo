13f192cc7b18b685b3f46bea501e055f5f6f6998
konurbaev-e <gameria@narod.ru>
2017-04-29 09:56:24 +0300

    first 2 classes

---
+++ Car.java
@@ -0,0 +1,5 @@
+package com.aurea.ca.onboarding.dead.code.detection.entity;
+
+public class Car {
+    private Person person;
+}
---
+++ Person.java
@@ -0,0 +1,5 @@
+package com.aurea.ca.onboarding.dead.code.detection.entity;
+
+public class Person {
+    private String firstName;
+}
